986c864e26882eaf71be978c14cda1167a00caa5
Léo <leo.dupuy21@gmail.com>
2024-11-29 00:02:23 +0100

    Fichier fini et fonctionnel

--- read_MNIST.c
+++ read_MNIST.c
@@ -20,12 +20,90 @@ Le set étant de taille convenable on va d'abord le stocker dans un premier temp
 */
 
 #include <stdio.h>
+#include "matrix.c"
 
 int read_32bits(FILE *fp){
     /*permet de retourner un numéro sur 32 bits interprété comme un entier*/
 
-    unsigned char *buffer[4];
+    unsigned char buffer[4];
     fread(buffer, 1, 4, fp);
 
-    return  *buffer[0] << 24 | *buffer[1] << 16 | *buffer[2] << 8 | *buffer[3];
+    return buffer[0] << 24 | buffer[1] << 16 | buffer[2] << 8 | buffer[3];
 }
+
+float read_byte(FILE *fp){
+    /*permet de retourner un numéro flotant sur 8 bits*/
+
+    unsigned char buffer[1];
+    fread(buffer, 1, 1, fp);
+
+    float retour = (float)buffer[0];
+
+    return retour;
+}
+
+int *get_data_mnist(FILE *fp){
+
+    int *output[3];
+    int validation;
+
+    validation = read_32bits(fp);
+    if(validation != 2051){
+        printf("Les données entrées ne sont pas des images...");
+        return NULL;
+    }
+
+    output[0] = read_32bits(fp);
+    output[1] = read_32bits(fp);
+    output[2] = read_32bits(fp);
+
+    return output;
+}
+
+void load_images(matrix *output, int nb, int width, int height, FILE *fp){
+
+    (*output).column = nb;
+    (*output).row = width*height;
+    
+    (*output).data = (float**)malloc( nb*sizeof(float *) );
+
+    for(int i = 0; i<nb; i++){
+        ((*output).data)[i] = (float*)malloc( height*width*sizeof(float));
+        for(int j = 0; j<width*height; j++){
+            ((*output).data[i])[j] = read_byte(fp);
+        }
+    }
+}
+
+void print_image_MNIST(matrix *input, int image_nb){
+    for( int j = 0; j<28; j++){
+        for(int i = 0; i<28; i++){
+            if((*input).data[image_nb][i+j*28] > 127){
+                printf("#");
+            }
+            else{
+                printf("0");
+            }
+        }
+        printf("\n");
+    }
+}
+
+void main(){
+
+    FILE *f = fopen("train-images.idx3-ubyte","rw");
+    read_32bits(f);
+    int nb = read_32bits(f);
+    printf("%d \n", nb);
+    int height= read_32bits(f);
+    printf("%d \n", height);
+    int width = read_32bits(f);
+    printf("%d \n", width);
+
+    matrix images_MNIST;
+    load_images(&images_MNIST,nb,width,height,f);
+
+    print_image_MNIST(&images_MNIST, 750);
+
+}
+
